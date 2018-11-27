fbc8481bbf0a27e49a5368f8b3837e988aec6be9
Drew Bragg <DRBragg@gmail.com>
2018-11-27 08:50:42 -0500

    added ash_contacts endpoint

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_24_203346) do
+ActiveRecord::Schema.define(version: 2018_11_26_235220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ash_contacts", force: :cascade do |t|
+    t.string "email"
+    t.string "trial"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "question", null: false
