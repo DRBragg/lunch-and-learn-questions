bed671496c21dacd2c2fc13c2daaa999c11a7b75
Drew Bragg <DRBragg@gmail.com>
2019-05-13 09:55:26 -0400

    changes per rick

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   resources :ash_contacts
   resources :questions
   root "home#index"
-  get "/team" => "home#team"
-  get "/team/rick" => "home#rick"
-  get "/team/craig" => "home#craig"
-  get "/team/drew" => "home#drew"
+  # get "/team" => "home#team"
+  # get "/team/rick" => "home#rick"
+  # get "/team/craig" => "home#craig"
+  # get "/team/drew" => "home#drew"
   post "/slack/questions" => "slack/questions#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
